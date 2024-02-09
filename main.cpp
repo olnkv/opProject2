a431dbbbdd4a9196dc87db1a6227b43ee595e833
olnkv <olenkovic.edvin@gmail.com>
2024-02-09 08:27:04 +0200

    Nuskaitymas

--- main.cpp
+++ main.cpp
@@ -2,15 +2,32 @@
 
 using namespace std;
 
-struct{
+const int N = 2;
+const int H = 10;
+
+struct User{
     string name;
     string surname;
-    int hwRes;
+    int hwRes[H];
     int exRes;
-} User;
+};
+
+void Read(User person[]);
 
 int main()
 {
+    User person[N];
 
     return 0;
+}
+
+void Read(User person[])
+{
+    for(int i = 0; i < N; i++)
+    {
+        cin>>person[i].name>>person[i].surname;
+        for(int j = 0; j < H; j++)
+            cin>>person[i].hwRes[j];
+        cin>>person[i].exRes;
+    }
 }
